ac1f3b23777b7c4dac3b77373ae48bad5df5c0ea
Efe G <98314362+gift56@users.noreply.github.com>
2025-02-12 06:25:01 -0600

    now user can be able to send multiple emails

--- src/app/api/user/send-invite/route.ts
+++ src/app/api/user/send-invite/route.ts
@@ -3,24 +3,28 @@ import axios from "axios";
 import { BASE_URL } from "@/services";
 
 export async function POST(req: NextRequest) {
-  const { email } = await req.json();
+  const { emails } = await req.json();
   const authId = req.headers.get("auth-user-id");
   const address = req.headers.get("address");
 
   try {
-    const response = await axios.post(
-      `${BASE_URL}/user/invite`,
-      {
-        email,
-      },
-      {
-        headers: {
-          "auth-user-id": authId as string,
-          Address: address as string,
-        },
-      }
+    const responsePromises = emails.map((email: string) =>
+      axios.post(
+        `${BASE_URL}/user/invite`,
+        { email },
+        {
+          headers: {
+            "auth-user-id": authId as string,
+            Address: address as string,
+          },
+        }
+      )
     );
-    return NextResponse.json(response.data);
+
+    const responses = await Promise.all(responsePromises);
+    const results = responses.map((res) => res.data);
+
+    return NextResponse.json(results);
   } catch (error) {
     console.error(error);
     return NextResponse.json(
